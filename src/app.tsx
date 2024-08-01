cf6dfd0b951cb7866c2064e7da38087be3afa0a0
Lucas Fidelis <lucasm241301@gmail.com>
2024-07-31 23:23:29 -0300

    Separa o primeiro card pra o componente marketingCard

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import { tv } from "tailwind-variants";
-import { Button } from "./components/button";
 import { Card } from "./components/cards";
 import { HeaderRoot } from "./components/header/HeaderRoot";
 import { Services } from "./components/services/services";
 import "./index.css"
+import { MarketingCard } from "./components/marketingCard";
 
 export const paragraph = tv({
   base: 'text-paragraph-mobile md:text-paragraph-desktop md:text-left ',
@@ -29,28 +29,11 @@ export function App() {
   return (
     <div className="flex flex-col w-full px-5 space-y-12 md:px-12 lg:px-24">
       <HeaderRoot></HeaderRoot>
-      <div className="
-        grid w-full gap-y-6 justify-items-start items-center
-        grid-areas-card-mobile
-       
-        lg:grid-areas-card-desktop"
-      >
-        <div className="
-          [grid-area:title]  
-          text-title-1-mobile 
-          md:text-title-1-desktop
-        ">
-          Navigating the digital landscape for success
-        </div>
-        <p className={`[grid-area:subtitle] ${paragraph({ size: "md", alignTextMobile: "left" })}`}>
-          Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
-        </p>
-        <Button />
-        <img src="/public/assets/megafone.svg" alt="" className="
-          [grid-area:image] 
-          w-fit place-self-center"
-        />
-      </div>
+      
+      <MarketingCard
+        paragraphSize="md"
+        alignTextMobile="left"
+      />
 
       <Services/>
 
---
+++ src/components/marketingCard.tsx
@@ -0,0 +1,33 @@
+import { paragraph } from "../app";
+import { Button } from "./button";
+
+interface MarketingCardProps {
+    paragraphSize?: "full" | "sm" | "md" | "lg" | undefined
+    alignTextMobile?: "center" | "left" | undefined
+}
+
+export function MarketingCard({ paragraphSize, alignTextMobile }: MarketingCardProps) {
+    return (
+        <div className="
+        grid w-full gap-y-6 justify-items-start items-center
+        grid-areas-card-mobile
+       
+        lg:grid-areas-card-desktop"
+        >
+            <div className="
+                [grid-area:title]  
+                text-title-1-mobile 
+                md:text-title-1-desktop
+            ">
+                Navigating the digital landscape for success
+            </div>
+            <p className={`[grid-area:subtitle] ${paragraph({ size: paragraphSize, alignTextMobile: alignTextMobile })}`}>
+                Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+            </p>
+            <Button />
+            <img src="/public/assets/megafone.svg" alt="" className="
+[grid-area:image] w-fit place-self-center"
+            />
+        </div>
+    )
+}
