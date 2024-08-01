0288719726bfb09f80e643029d2357b3ca5766cb
Lucas Fidelis <lucasm241301@gmail.com>
2024-08-01 00:32:08 -0300

    Adiciona elemento de Marquee entre os cards de marketing e serviços

--- src/app.tsx
+++ src/app.tsx
@@ -4,24 +4,25 @@ import { HeaderRoot } from "./components/header/HeaderRoot";
 import { Services } from "./components/services/services";
 import "./index.css"
 import { MarketingCard } from "./components/marketingCard";
+import Marquee from "react-fast-marquee";
 
 export const paragraph = tv({
   base: 'text-paragraph-mobile md:text-paragraph-desktop md:text-left ',
   variants: {
-      size: {
-          sm: 'w-full md:w-[23vw]',
-          md: 'w-full md:w-[34vw]',
-          lg: 'w-full md:w-[42vw]',
-          full: 'w-full'
-      },
-      alignTextMobile: {
-        left: 'text-left',
-        center: 'text-center'
-      }
+    size: {
+      sm: 'w-full md:w-[23vw]',
+      md: 'w-full md:w-[34vw]',
+      lg: 'w-full md:w-[42vw]',
+      full: 'w-full'
+    },
+    alignTextMobile: {
+      left: 'text-left',
+      center: 'text-center'
+    }
   },
-  defaultVariants:{
-      size: 'full',
-      alignTextMobile: 'center'
+  defaultVariants: {
+    size: 'full',
+    alignTextMobile: 'center'
   }
 })
 
@@ -29,13 +30,28 @@ export function App() {
   return (
     <div className="flex flex-col w-full px-5 space-y-12 md:px-12 lg:px-24">
       <HeaderRoot></HeaderRoot>
-      
+
       <MarketingCard
         paragraphSize="md"
         alignTextMobile="left"
       />
 
-      <Services/>
+      <Marquee>
+        <div className="flex flex-wrap lg:flex-nowrap grayscale">
+          <div className="flex items-center gap-9 lg:gap-24">
+            <img src="/public/assets/logos-marquee/amazon.svg" alt="" className="w-fit h-fit lg:ml-24" />
+            <img src="/public/assets/logos-marquee/dribbble.svg" alt="" className="w-fit h-fit" />
+            <img src="/public/assets/logos-marquee/hubspot.svg" alt="" className="w-fit h-fit" />
+          </div>
+          <div className="flex items-center gap-9 lg:gap-24">
+            <img src="/public/assets/logos-marquee/notion.svg" alt="" className="w-fit h-fit ml-9 lg:ml-24" />
+            <img src="/public/assets/logos-marquee/netflix.svg" alt="" className="w-fit h-fit" />
+            <img src="/public/assets/logos-marquee/zoom.svg" alt="" className="w-fit h-fit" />
+          </div>
+        </div>
+      </Marquee>
+
+      <Services />
 
       <Card.Header
         title="Case Studies"
