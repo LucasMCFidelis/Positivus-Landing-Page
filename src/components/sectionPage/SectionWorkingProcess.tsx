895717fdc188b2cb357197edeb420831c1eb1397
Lucas Fidelis <lucasm241301@gmail.com>
2024-08-09 16:26:27 -0300

    Finaliza SectionworkingProcess, adicionando um map para criação dos cards

--- src/components/sectionPage/SectionWorkingProcess.tsx
+++ src/components/sectionPage/SectionWorkingProcess.tsx
@@ -6,20 +6,22 @@ import { paragraph } from "../../app";
 
 interface CardWorkingProcessProps {
     indexCard: number
-
+    step: string
+    description: string
 }
 
-
 export function SectionWorkingProcess() {
-    const CardWorkingProcess = (({ indexCard }: CardWorkingProcessProps) => (
+    const CardWorkingProcess = (({ indexCard, step, description }: CardWorkingProcessProps) => (
         <Card.Wrapper bgColor={openCardIndex === indexCard ? "secondary" : "primary"} className="space-y-4">
-            <div className="flex justify-between items-center gap-6">
-                <h2 className="text-title-2-mobile lg:text-title-2-desktop">
-                    {indexCard+1}
-                </h2>
-                <h3 className="text-title-3-mobile lg:text-title-3-desktop flex-indexCard">
-                    Consultation
-                </h3>
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-6">
+                    <h2 className="text-title-2-mobile lg:text-title-2-desktop">
+                        {(indexCard+1).toString().padStart(2, '0')}
+                    </h2>
+                    <h3 className="text-title-3-mobile lg:text-title-3-desktop flex-indexCard">
+                        {step}
+                    </h3>
+                </div>
                 <Button
                     colorBG={"white"}
                     rounded="full"
@@ -32,7 +34,7 @@ export function SectionWorkingProcess() {
                 <>
                     <div className="h-0.5 w-full bg-zinc-900"></div>
                     <p className={paragraph({ size: "full" })}>
-                        During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.
+                        {description}
                     </p>
                 </>
             )}
@@ -53,8 +55,34 @@ export function SectionWorkingProcess() {
                 paragraphSize="sm"
             />
             <div className="space-y-8">
-                <CardWorkingProcess indexCard={0}/>
-                <CardWorkingProcess indexCard={1}/>
+                {[
+                    {
+                        step: 'Consultation',
+                        description: 'During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.'
+                    },
+                    {
+                        step: 'Research and Strategy Development',
+                        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deserunt iure deleniti officia quasi quisquam harum, facilis eum! Maxime mollitia dolore temporibus facere ducimus ut consequatur iste in tempore facilis!'
+                    },
+                    {
+                        step: 'Implementation',
+                        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deserunt iure deleniti officia quasi quisquam harum, facilis eum! Maxime mollitia dolore temporibus facere ducimus ut consequatur iste in tempore facilis!'
+                    },
+                    {
+                        step: 'Monitoring and Optimization',
+                        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deserunt iure deleniti officia quasi quisquam harum, facilis eum! Maxime mollitia dolore temporibus facere ducimus ut consequatur iste in tempore facilis!'
+                    },
+                    {
+                        step: 'Reporting and Communication',
+                        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deserunt iure deleniti officia quasi quisquam harum, facilis eum! Maxime mollitia dolore temporibus facere ducimus ut consequatur iste in tempore facilis!'
+                    },
+                    {
+                        step: 'Continual Improvement',
+                        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deserunt iure deleniti officia quasi quisquam harum, facilis eum! Maxime mollitia dolore temporibus facere ducimus ut consequatur iste in tempore facilis!'
+                    },
+                ].map((item, idx) => (
+                    <CardWorkingProcess key={idx} indexCard={idx} step={item.step} description={item.description}/>
+                ))}
             </div>
         </>
     )
