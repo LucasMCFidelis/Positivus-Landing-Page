27fde70136fe5e5aac19bf71fa44e63b00fabb60
Lucas Fidelis <lucasm241301@gmail.com>
2024-08-11 16:58:02 -0300

    Organiza a SectioncontactUs

--- src/components/sectionPage/SectionContactUs.tsx
+++ src/components/sectionPage/SectionContactUs.tsx
@@ -1,7 +1,7 @@
 import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 import { Button } from "../button";
 import { Card } from "../cards";
-import {twMerge} from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge'
 
 interface ContactUsInputProps extends InputHTMLAttributes<HTMLInputElement> {
     title: string
@@ -9,12 +9,14 @@ interface ContactUsInputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const ContactUsInput = ({ title, ...rest }: ContactUsInputProps) => (
     <div className="flex flex-col gap-2">
-        <label htmlFor={title.toLowerCase()}>{title + (rest.required === true ? "*" : "")}</label>
+        <label htmlFor={title.toLowerCase()}>
+            {title + (rest.required === true ? "*" : "")}
+        </label>
         <input
             {...rest}
             id={title.toLowerCase()}
             placeholder={title}
-            className={twMerge("rounded-xl px-8 py-4 border border-zinc-900 w-full h-[8vh] resize-none ", rest.className)} 
+            className={twMerge("rounded-xl px-8 py-4 border border-zinc-900 w-full h-[8vh] resize-none ", rest.className)}
         />
     </div>
 )
@@ -25,12 +27,14 @@ interface ContactUsTextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElem
 
 const ContactUsTextarea = ({ title, ...rest }: ContactUsTextareaProps) => (
     <div className="flex flex-col gap-2">
-        <label htmlFor={title.toLowerCase()}>{title + (rest.required === true ? "*" : "")}</label>
+        <label htmlFor={title.toLowerCase()}>
+            {title + (rest.required === true ? "*" : "")}
+        </label>
         <textarea
             {...rest}
             id={title.toLowerCase()}
             placeholder={title}
-            className={twMerge("rounded-xl px-8 py-4 border border-zinc-900 w-full h-[20vh] resize-none ", rest.className)} 
+            className={twMerge("rounded-xl px-8 py-4 border border-zinc-900 w-full h-[20vh] resize-none ", rest.className)}
         />
     </div>
 )
@@ -44,17 +48,37 @@ export function SectionContactUs() {
                 paragraphSize="sm"
             />
 
-            <Card.Wrapper bgColor="primary" border="none" rounded="lg" className="relative w-full h-[80vh] grid grid-cols-1 lg:grid-cols-2">
-                <form action="" className="flex flex-col text-paragraph-mobile lg:text-paragraph-desktop gap-5 p-8">
-                    <ContactUsInput title={"Name"} />
-                    <ContactUsInput title={"Email"} required />
-                    <ContactUsTextarea title={"Message"} required />
+            <Card.Wrapper
+                bgColor="primary"
+                border="none"
+                rounded="lg"
+                className="relative w-full grid grid-cols-1 lg:grid-cols-2"
+            >
+                <form action="" className="flex flex-col text-paragraph-mobile lg:text-paragraph-desktop gap-5 lg:p-8">
+                    <ContactUsInput
+                        title={"Name"}
+                    />
+                    <ContactUsInput
+                        title={"Email"}
+                        required
+                    />
+                    <ContactUsTextarea
+                        title={"Message"}
+                        required
+                    />
 
-                    <Button colorBG={"dark"} text="Send Message" type="submit" />
+                    <Button
+                        colorBG={"dark"}
+                        text="Send Message"
+                        type="submit"
+                    />
                 </form>
-                <img src="/public/assets/contactUsImage.svg" alt="Contact Us" className="absolute hidden h-4/5 w-3/5 lg:block top-[10%] -right-[28%]" />
+                <img
+                    src="/public/assets/contactUsImage.svg"
+                    alt="Contact Us"
+                    className="absolute hidden h-4/5 w-3/5 lg:block top-[10%] -right-[28%]"
+                />
             </Card.Wrapper>
-
         </>
     )
 }
