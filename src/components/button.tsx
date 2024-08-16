5614f798d85abcfd556a2bbd98bea04a64d417f1
Lucas Fidelis <lucasm241301@gmail.com>
2024-08-16 14:25:10 -0300

    Adiciona variante para borda do componente Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,35 +6,41 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string,
   colorBG: "dark" | "green" | "white" | "transparent"
   rounded?: "sm" | "full"
+  border?: boolean
   children?: ReactNode
 }
 
-export function Button({ text, colorBG, rounded, children, className, ...rest }: ButtonProps) {
+export function Button({ text, colorBG, rounded, border, children, className, ...rest }: ButtonProps) {
   const button = tv({
-    base:`text-paragraph-mobile lg:text-paragraph-desktop`,
+    base: 'text-paragraph-mobile lg:text-paragraph-desktop',
     variants: {
       color: {
         dark: 'bg-[#191A23] text-gray-200',
         green: 'bg-[#B9FF66]',
-        white: 'bg-[#f3f3f3] border-2 border-zinc-950',
-        transparent: 'bg-transparent border-2 border-zinc-950'
+        white: 'bg-[#f3f3f3]',
+        transparent: 'bg-transparent'
       },
       rounded: {
         sm: 'rounded-xl py-5 px-8 w-full lg:w-fit ',
         full: 'rounded-full p-2'
+      },
+      border: {
+        true: 'border-2 border-zinc-950',
+        false: 'border-none'
       }
     },
     defaultVariants: {
       color: 'dark',
-      rounded: 'sm'
+      rounded: 'sm',
+      border: false
     }
   })
   return (
     <button
       {...rest}
-      className={twMerge(button({ color: colorBG, rounded: rounded }), className, rest.disabled === true ? "opacity-50" : "")}
+      className={twMerge(button({ color: colorBG, rounded: rounded, border: border }), className, rest.disabled === true ? "opacity-50" : "")}
     >
-      {text} 
+      {text}
       {children}
     </button>
   )
